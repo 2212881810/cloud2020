a779862cd41081969ffe83255368e3fd79d13a86
biyu.zeng <biyu.zeng@heweisoft.com>
2020-03-22 22:58:09 +0800

    'ignore'

--- cloud-eureka-server7001/src/main/java/qinfeng/zheng/eureka7001/Eureka7001App.java
+++ cloud-eureka-server7001/src/main/java/qinfeng/zheng/eureka7001/Eureka7001App.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class Eureka7001App {
+
     public static void main(String[] args) {
         SpringApplication.run(Eureka7001App.class, args);
+        System.out.println("eureka注册中心启动成功");
     }
 }
