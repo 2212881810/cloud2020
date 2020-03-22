a677034cf3d3aac10ef6f424bb71aaec904abe37
biyu.zeng <biyu.zeng@heweisoft.com>
2020-03-22 22:45:18 +0800

    '搭建eureka集群'

--- cloud-eureka-server7002/src/main/java/qinfeng/zheng/eureka7002/Eureka7002App.java
+++ cloud-eureka-server7002/src/main/java/qinfeng/zheng/eureka7002/Eureka7002App.java
@@ -1,4 +1,4 @@
-package qinfeng.zheng.eureka;
+package qinfeng.zheng.eureka7002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApp {
+public class Eureka7002App {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApp.class, args);
+        SpringApplication.run(Eureka7002App.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/qinfeng/zheng/payment/PaymentApp.java
+++ cloud-provider-payment8001/src/main/java/qinfeng/zheng/payment/PaymentApp.java
@@ -2,7 +2,9 @@ package qinfeng.zheng.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentApp {
     public static void main(String[] args) {
